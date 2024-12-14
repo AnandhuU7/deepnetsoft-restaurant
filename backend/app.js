541ecab4ed5c50503636c1ff7a7fd1bfc05e52b1
AnandhuU7 <anandhuachu777.com>
2024-12-14 09:17:05 +0530

    cors updated

--- backend/app.js
+++ backend/app.js
@@ -8,35 +8,35 @@ const authRoutes = require('./routes/authRoutes');
 const menuRoutes = require('./routes/menuRoutes');
 const bodyParser = require('body-parser');
 
+
 connectDB();
 
 const app = express();
 
+
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || origin === 'http://localhost:5173' || origin === 'https://eclectic-faloodeh-3798a0.netlify.app') {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+      origin: 'https://lovely-biscuit-d25443.netlify.app',
+      methods: 'GET,POST,PUT,DELETE',
+      allowedHeaders: 'Content-Type,Authorization',
   })
 );
 
+
 app.use(bodyParser.json());
+
 
 app.use('/auth', authRoutes);
 app.use('/menus', menuRoutes);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+
 process.on('SIGINT', () => {
-  mongoose.connection.close(() => {
-    console.log('MongoDB connection closed due to app termination');
-    process.exit(0);
-  });
+    mongoose.connection.close(() => {
+        console.log('MongoDB connection closed due to app termination');
+        process.exit(0);
+    });
 });
